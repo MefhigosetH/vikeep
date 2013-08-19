4eb9ad53006c2fc3e2fcdf88f6456da5efafd870
MefhigosetH <mefhigoseth@gmail.com>
2013-08-19 00:29:22 -0300

    Fixed the home link at navbar. Close #4.

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ Legal notice:
 	<div class="navbar-inner">
 		<a class="brand" href=".">Vikeep</a>
 		<ul class="nav">
-			<li class="active"><a href="#">Home</a></li>
+			<li class="active"><a href="/">Home</a></li>
 			<li class="active"><a href="https://github.com/MefhigosetH/vikeep">GitHub</a></li>
 		</ul>
 		<p class="navbar-text pull-right">Beta</p>
