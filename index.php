7f3761510bed3e70392adb3607e6839955a2b85c
MefhigosetH <mefhigoseth@gmail.com>
2014-05-18 02:17:19 -0300

    Fixed subtitles URL for Pt language.

--- index.php
+++ index.php
@@ -69,12 +69,12 @@ if( isset($_GET['episode']) && !empty($_GET['episode']) ) {
 		echo "<h3>1. Download subtitles. Choose your lang:</h3>";
 		echo "<p>Right-click -> Save as...</p>";
 		$strEsUrl = $viki->subtitles($_GET['episode'],"es");
-		$strEnUrl = $viki->subtitles($_GET['episode'],"pt");
+		$strPtUrl = $viki->subtitles($_GET['episode'],"pt");
 		$strEnUrl = $viki->subtitles($_GET['episode'],"en");
 		$strFrUrl = $viki->subtitles($_GET['episode'],"fr");
 		echo "<p>";
 		echo "<a href='".$strEsUrl."' title='Download Spanish subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> Spanish</a>";
-		echo " <a href='".$strEnUrl."' title='Download Português subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> Português</a>";
+		echo " <a href='".$strPtUrl."' title='Download Português subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> Português</a>";
 		echo " <a href='".$strFrUrl."' title='Download French subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> French</a>";
 		echo " <a href='".$strEnUrl."' title='Download English subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> English</a>";
 		echo "</p>";
