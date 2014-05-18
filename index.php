8f3d2a5164c979e3df58513c91d8abdace86bdfa
MefhigosetH <mefhigoseth@gmail.com>
2014-05-18 01:48:03 -0300

    Fixed typo from @magalmix/patch-1

--- index.php
+++ index.php
@@ -72,10 +72,12 @@ if( isset($_GET['episode']) && !empty($_GET['episode']) ) {
 		$strEnUrl = $viki->subtitles($_GET['episode'],"pt");
 		$strEnUrl = $viki->subtitles($_GET['episode'],"en");
 		$strFrUrl = $viki->subtitles($_GET['episode'],"fr");
-		echo "<p><a href='".$strEsUrl."' title='Download Spanish subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> Spanish</a>";
-		echo " <a href='".$strEnUrl."' title='Download Português subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> Português</a></p>";
+		echo "<p>";
+		echo "<a href='".$strEsUrl."' title='Download Spanish subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> Spanish</a>";
+		echo " <a href='".$strEnUrl."' title='Download Português subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> Português</a>";
 		echo " <a href='".$strFrUrl."' title='Download French subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> French</a>";
-		echo " <a href='".$strEnUrl."' title='Download English subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> English</a></p>";
+		echo " <a href='".$strEnUrl."' title='Download English subtitles' class='btn btn-large btn-primary'><i class='icon-list-alt icon-white'></i> English</a>";
+		echo "</p>";
 
 		echo "<h3>2. Download video. Choose your quality:</h3>";
 		echo "<p>Right-click -> Save as...</p>";
