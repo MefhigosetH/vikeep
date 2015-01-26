9a60a49597eecdddf46e3b936eb9d090f494877f
MefhigosetH <mefhigoseth@gmail.com>
2015-01-26 04:15:04 -0300

    Added navbar.inc.php

--- index.php
+++ index.php
@@ -31,20 +31,9 @@ include('functions.inc.php');
 
 <body>
 
-<!-- NavBar -->
-<div class="navbar navbar-inverse">
-	<div class="navbar-inner">
-		<a class="brand" href=".">Vikeep</a>
-		<ul class="nav">
-			<li class="active"><a href="/"><i class='icon-home'></i> Home</a></li>
-			<li class="active"><a href="https://github.com/MefhigosetH/vikeep">GitHub</a></li>
-		</ul>
-		<p class="navbar-text pull-right">Beta</p>
-	</div>
-</div>
+<?php include('navbar.inc.php'); ?>
 
 <div class="container">
-
 <?php
 if( isset($_GET['episode']) && !empty($_GET['episode']) ) {
 	$viki = new vikiAPI();
---
+++ navbar.inc.php
@@ -0,0 +1,12 @@
+<!-- NavBar start -->
+<div class="navbar navbar-inverse">
+	<div class="navbar-inner">
+		<a class="brand" href=".">Vikeep</a>
+		<ul class="nav">
+			<li class="active"><a href="/"><i class='icon-home'></i> Home</a></li>
+			<li class="active"><a href="https://github.com/MefhigosetH/vikeep">GitHub</a></li>
+		</ul>
+		<p class="navbar-text pull-right">Beta</p>
+	</div>
+</div>
+<!-- NavBar end -->
