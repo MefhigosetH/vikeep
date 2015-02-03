e6dd2a9ad069eddb66220c5072e55834432c9df6
MefhigosetH <mefhigoseth@gmail.com>
2015-02-03 04:08:30 -0300

    Added Propeller metatag.

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -4,6 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Vikeep - Keep viki.com videos, yours">
 <meta name="author" content="MefhigosetH">
+<meta name="propeller" content="e7e0a4bb2644c6d62e680d9e6d6b0ceb" />
 <!-- Bootstrap -->
 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
